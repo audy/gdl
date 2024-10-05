06ead7962796ac09e5e4dab18ac1e219f9f48636
Austin Richardson <accounts@agdr.org>
2024-10-04 21:02:10 -0700

    More progress bar stuff

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,22 @@ use reqwest::Client;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
+use std::sync::Arc;
 use taxonomy::ncbi::load;
 use taxonomy::Taxonomy;
+use tokio::sync::Semaphore;
 use tokio::task;
 
 const ASSEMBLY_SUMMARY_PATH: &str = "assembly_summary_refseq.txt";
 const TAXDUMP_DIR: &str = "taxdump";
 
-const TARGET_TAX_ID: &str = "357276"; // Phocaeicola dorei
+const TARGET_TAX_ID: &str = "821"; // Phocaeicola dorei
 
 #[derive(Debug, serde::Deserialize)]
 struct NCBIAssembly {
     taxid: String,
     ftp_path: String,
+    asm_name: String,
 }
 
 type BoxedError = Box<dyn Error + Send + Sync + 'static>;
@@ -27,7 +30,7 @@ async fn download_assembly(
     assembly: &NCBIAssembly,
     pb: ProgressBar,
 ) -> Result<(), BoxedError> {
-    pb.set_message(format!("Starting {}", assembly.ftp_path));
+    pb.set_message(format!("Starting {}", assembly.asm_name));
 
     let last_part = assembly
         .ftp_path
@@ -50,7 +53,7 @@ async fn download_assembly(
         file.write_all(&chunk)?;
     }
 
-    pb.finish_with_message(format!("Complete {}", assembly.ftp_path));
+    pb.finish_and_clear();
     Ok(())
 }
 
@@ -59,16 +62,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let assembly_summary_file = File::open(ASSEMBLY_SUMMARY_PATH)?;
 
     // this seems to use the least amount of memory out of all the formats
+    println!("Loading taxonomy from {}...", TAXDUMP_DIR);
     let tax = load(TAXDUMP_DIR)?;
 
     let descendant_tax_ids = tax.descendants(TARGET_TAX_ID)?;
 
     println!(
-        "Found {} descendants of tax ID {} ({})",
+        "Found {} descendants of {} ({})...",
         descendant_tax_ids.len(),
-        TARGET_TAX_ID,
         tax.name(TARGET_TAX_ID)?,
+        TARGET_TAX_ID
     );
+
+    println!("Reading assembly summaries from {}", ASSEMBLY_SUMMARY_PATH,);
 
     // skip first line because it doesn't contain an actual header
     let mut buf_reader = BufReader::new(assembly_summary_file);
@@ -82,6 +88,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut assemblies: Vec<NCBIAssembly> = Vec::new();
 
+    // todo: start downloading Assemblies immediately
     for result in reader.deserialize() {
         let assembly: NCBIAssembly = result?;
         if descendant_tax_ids.contains(&assembly.taxid.as_str()) {
@@ -92,16 +99,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Downloading {} assemblies", assemblies.len());
 
+    // TODO: one master progress bar with ETA?
+
     let multi_process = MultiProgress::new();
     let progress_bars: Vec<_> = assemblies
         .into_iter()
         .map(|assembly| {
             let pb = multi_process.add(ProgressBar::new(0));
             pb.set_style(
-                ProgressStyle::with_template(
-                    "{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes}"
-                ).unwrap()
-                 .progress_chars("#>-"));
+                ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{bar:.cyan/blue}] {bytes}/{total_bytes} - {msg}")
+                    .unwrap()
+                    .progress_chars("#>-"),
+            );
             (assembly, pb)
         })
         .collect();
@@ -109,11 +118,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Download assemblies in parallel
     let client = Client::new();
 
+    let semaphore = Arc::new(Semaphore::new(12));
+
     let tasks: Vec<_> = progress_bars
         .into_iter()
         .map(|(assembly, pb)| {
             let client = client.clone();
-            task::spawn(async move { download_assembly(&client, &assembly, pb).await })
+            let semaphore = semaphore.clone();
+            task::spawn(async move {
+                let _permit = semaphore.acquire().await.unwrap();
+                download_assembly(&client, &assembly, pb).await
+            })
         })
         .collect();
 
